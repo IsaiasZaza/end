9dfa821fcb9b211f6a97295de181e9152afed830
Isaias Gonçalves <131933508+IsaiasZaza@users.noreply.github.com>
2024-11-19 22:54:45 -0300

    feat: created

--- prisma/migrations/20241119164847_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Gift" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "image" TEXT NOT NULL,
-    "nome" TEXT NOT NULL,
-    "quantidade" INTEGER NOT NULL
-);
